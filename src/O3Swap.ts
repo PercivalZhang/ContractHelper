82d7bdab7b8d6074d452a942536c0750e1fda013
mingqi <zhangmingqi@huobi.com>
2021-09-08 18:38:09 +0800

    update

--- src/O3Swap.ts
+++ src/O3Swap.ts
@@ -13,14 +13,31 @@ const logger = LoggerFactory.getInstance().getLogger('main');
 const o3TokenAddress = '0xee9801669c6138e84bd50deb500827b776777d28';
 
 /**
+ * 以HECO上的O3Swap为例：
+ *
+ * Vault部分：
  * 这两个vault质押产生的奖励，领取后将进入锁定状态，必须通过向第三个vault中质押O3-HUSD LP来解锁奖励
- * vault O3: 0xa36f7852114b5a907cfb0246949e432e67bf2126
- * vault BTC: 0x0423f3b26593db401a3382b483981e619c808dfc
+ * - vault O3: 0xa36f7852114b5a907cfb0246949e432e67bf2126
+ * - vault LP-BTC: 0x0423f3b26593db401a3382b483981e619c808dfc //BTC HUB LP质押挖矿
  *
  * 该vault用于解锁上面两个vault质押收获的奖励，解锁速度跟质押的O3-HUSD LP数量有关
  * vault LP O3-HUSD(0xa83925175646527cd5f3c0ffe0f39a6c32af32fb): 0xee9801669c6138e84bd50deb500827b776777d28
  *
- * HUB BTC: 0xd98ee7ca1b33e60c75e3cd9493c566fc857592c8
+ * HUB部分
+ * 介绍：HUB用于跨链资产的接入，目前提供四个HUB Pools，每一个都是3币池子：
+ * - O3-Pool：将BUSD从BSC跨链到Ether，将HUSD从HECO跨链到Ether，Ether的USDT
+ * - ETH-Pool：将ETH从Ether(WETH)/HECO跨链到BSC，BSC的ETH
+ * - BTC-Pool：将BTC从Ether(WBTC)/BSC(BTCB)跨链到HECO， HECO的HBTC
+ *   - LP-BTC：0x3E95b3049c23aD5207DDa3ca5e19cD54242ad541
+ * - USDC-Pool：将USDC从Ether/BSC跨链到Polygon，HECO的USDC
+ *
+ * 对应与当前O3Swap链接的网络，关注的HUB分别是：
+ * HECO： HUB-BTC-3Pool
+ * BSC： HUB-ETH-3Pool
+ * Polygon： HUB-USDC-3Pool
+ * ETH： HUB O3-3Pool
+ *
+ * 同样vault也随网络差异，所支持质押挖矿的LP也会相对应变化。
  */
 
 const getSingleVaultDetail = async (vaultAddress: string, userAddress: string) => {
@@ -89,6 +106,13 @@ const getLPVaultDetail = async (vaultAddress: string, lptAddress: string, userAd
             .toNumber()} ${o3Token.symbol}/Block`,
     );
 };
+const getHUBPoolDetail = async (hubLPAddress: string, userAddress: string) => {
+    const lpt = new ContractHelper(hubLPAddress, './erc20.json', network);
+    lpt.toggleHiddenExceptionOutput();
+
+    const balance = await lpt.callReadMethod('balanceOf', userAddress);
+    logger.info(balance);
+};
 
 const main = async () => {
     await getSingleVaultDetail(
@@ -106,6 +130,8 @@ const main = async () => {
         '0xa83925175646527cd5f3c0ffe0f39a6c32af32fb',
         '0xD2050719eA37325BdB6c18a85F6c442221811FAC',
     ); // get vault - O3-HUSD LP receipt
+    logger.info(`==============================================`);
+    await getHUBPoolDetail('0x3E95b3049c23aD5207DDa3ca5e19cD54242ad541', '0xD2050719eA37325BdB6c18a85F6c442221811FAC');
 };
 
 main().catch((e) => {
---
+++ src/heco.vLoot.ts
@@ -0,0 +1,46 @@
+import { ContractHelper } from './library/contract.helper';
+import { LoggerFactory } from './library/LoggerFactory';
+import { NetworkType } from './library/web3.factory';
+import { SwissKnife, Token } from './library/swiss.knife';
+import BigNumber from 'bignumber.js';
+
+interface LootMetadata {
+    name: string;
+    description: string;
+    image: string;
+}
+
+const network = NetworkType.HECO;
+
+const swissKnife = new SwissKnife(network);
+
+const logger = LoggerFactory.getInstance().getLogger('main');
+
+const vLoot = new ContractHelper('0xdbd67269b76f89cfb7b79dacba3105fd6b982c68', './heco.vLoot/vLoot.json', network);
+vLoot.toggleHiddenExceptionOutput();
+
+const decodeTokenURI = (tokenURI: string): LootMetadata => {
+    const data = JSON.parse(Buffer.from(tokenURI.toString().substr(29), 'base64').toString());
+    const metadata: LootMetadata = {
+        name: data.name,
+        description: data.description,
+        image: data.image,
+    };
+    return metadata;
+};
+
+const decodeImage = (imageData: string): string => {
+    return Buffer.from(imageData.toString().substr(26), 'base64').toString();
+};
+
+const main = async () => {
+    const tokenURI = await vLoot.callReadMethod('tokenURI', 77);
+    const lootMetadata = decodeTokenURI(tokenURI);
+    logger.info(lootMetadata);
+    const imageData = decodeImage(lootMetadata.image);
+    logger.info(imageData);
+};
+
+main().catch((e) => {
+    logger.error(e.message);
+});
