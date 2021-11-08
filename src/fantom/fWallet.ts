faeedd769b5a157e2e4bc7917c9a4d3206a8f82b
PercivalZhang <36841424+PercivalZhang@users.noreply.github.com>
2021-11-08 21:40:21 +0800

    update fwallet

--- src/fantom/fWallet.ts
+++ src/fantom/fWallet.ts
@@ -18,52 +18,97 @@ const logger = LoggerFactory.getInstance().getLogger('main');
  */
 const Config = {
     fantomMint: '0xbb634cafef389cdd03bb276c82738726079fcf2e',
-    wFTM: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
-    fToken: {
+    ftmMintTokenRegistry: '0x5ac50e414bb625ce7dc17ad165a604bf3ca8fd23',
+    collateral: {
+        wFTM: '0x21be370d5312f44cb42ce377bc9b8a0cef1a4c83',
+        sFTM: '0x69c744d3444202d35a2783929a0f930f2fbb05ad',
+    },
+    fUSD: '0xad84341756bf337f5a0164515b1f6f993d194e1f',
+    fTokens: {
         fUSD: '0xad84341756bf337f5a0164515b1f6f993d194e1f',
+        fEUR: '0xe105621721d1293c27be7718e041a4ce0ebb227e',
     },
 };
 
-const main = async () => {
-    const wFTM = await swissKnife.syncUpTokenDB(Config.wFTM);
-    const fUSD = await swissKnife.syncUpTokenDB(Config.fToken.fUSD);
+const tokenRegistry = async () => {
+    const registry = new ContractHelper(Config.ftmMintTokenRegistry, './fWallet/ftm.mint.token.registry.json', network);
+    registry.toggleHiddenExceptionOutput();
+
+    const tokenCount = await registry.callReadMethod('tokensCount');
+    for (let i = 0; i < tokenCount; i++) {
+        const fTokenAddress = await registry.callReadMethod('tokensList', i);
+        const fTokenInfo = await registry.callReadMethod('tokens', fTokenAddress);
+        logger.info(`${fTokenInfo.symbol} : isActive[${fTokenInfo.isActive}] canMint[${fTokenInfo.canMint}]`);
+    }
+};
+const getCollateralDebtReceipt = async (userAddress: string) => {
+    const fUSD = await swissKnife.syncUpTokenDB(Config.fUSD);
 
     const ftmMint = new ContractHelper(Config.fantomMint, './fWallet/fantom.mint.json', network);
     ftmMint.toggleHiddenExceptionOutput();
 
+    //抵押池地址
     const collateralPoolAddress = await ftmMint.callReadMethod('getCollateralPool');
+    //债务池地址
     const debtPoolAddress = await ftmMint.callReadMethod('getDebtPool');
-
+    //加载抵押池合约
     const collateralPool = new ContractHelper(collateralPoolAddress, './fWallet/ftm.token.storage.json', network);
     collateralPool.toggleHiddenExceptionOutput();
+    //加载债务池合约
     const debtPool = new ContractHelper(debtPoolAddress, './fWallet/ftm.token.storage.json', network);
     debtPool.toggleHiddenExceptionOutput();
+    //抵押资产总价值（fUSD计价）
+    let totalCollateralValue = new BigNumber(0);
+    //遍历抵押资产token列表
+    for (const [key, cTokenAddress] of Object.entries(Config.collateral)) {
+        //获取目标用户在目标抵押资产token上的抵押数量
+        const collateralBalance = new BigNumber(
+            await collateralPool.callReadMethod('balance', userAddress, cTokenAddress),
+        );
+        if (collateralBalance.gt(0)) {
+            const cToken = await swissKnife.syncUpTokenDB(cTokenAddress);
+            logger.info(
+                `my collateral balance: ${collateralBalance
+                    .dividedBy(Math.pow(10, cToken.decimals))
+                    .toNumber()
+                    .toFixed(6)} ${cToken.symbol}`,
+            );
+            //获取抵押token的价格（fUSD计价）
+            const priceCToken = await ftmMint.callReadMethod('getPrice', cTokenAddress);
+            //计算抵押资产token的价值
+            const collateralValue = collateralBalance
+                .multipliedBy(priceCToken)
+                .dividedBy(Math.pow(10, cToken.decimals));
+            //累加
+            totalCollateralValue = totalCollateralValue.plus(collateralValue);
+        }
+    }
+    //铸造债务的计算方式同抵押资产类似
+    let totalDebtValue = new BigNumber(0);
+    for (const [key, fTokenAddress] of Object.entries(Config.fTokens)) {
+        const debtBalance = new BigNumber(await debtPool.callReadMethod('balance', userAddress, fTokenAddress));
+        if (debtBalance.gt(0)) {
+            const fToken = await swissKnife.syncUpTokenDB(fTokenAddress);
+            logger.info(
+                `my debt balance: ${debtBalance.dividedBy(Math.pow(10, fToken.decimals)).toNumber().toFixed(6)} ${
+                    fToken.symbol
+                }`,
+            );
+            const priceFToken = await ftmMint.callReadMethod('getPrice', fTokenAddress);
+            const debtValue = debtBalance.multipliedBy(priceFToken).dividedBy(Math.pow(10, fToken.decimals));
+            totalDebtValue = totalDebtValue.plus(debtValue);
+        }
+    }
 
-    const collateralBalance = new BigNumber(
-        await collateralPool.callReadMethod('balance', '0x228f23A962D1ACabB1775E31FAF7D1B5bfa85B5E', wFTM.address),
-    );
-    logger.info(
-        `my collateral balance: ${collateralBalance.dividedBy(Math.pow(10, wFTM.decimals)).toNumber().toFixed(6)} ${
-            wFTM.symbol
-        }`,
-    );
-    const debtFUSDBalance = new BigNumber(
-        await debtPool.callReadMethod('balance', '0x228f23A962D1ACabB1775E31FAF7D1B5bfa85B5E', fUSD.address),
-    );
-    logger.info(
-        `my debt balance: ${debtFUSDBalance.dividedBy(Math.pow(10, fUSD.decimals)).toNumber().toFixed(6)} ${
-            fUSD.symbol
-        }`,
-    );
+    if (totalCollateralValue.gt(0) && totalDebtValue.gt(0)) {
+        const cRatio = totalCollateralValue.dividedBy(totalDebtValue);
+        logger.info(`cRatio[Collateral/Debt]: ${cRatio.toNumber().toFixed(4)}`);
+    }
+};
 
-    const priceWFTM = await ftmMint.callReadMethod('getPrice', wFTM.address);
-    const priceFUSD = await ftmMint.callReadMethod('getPrice', fUSD.address);
-
-    const collateralValue = collateralBalance.multipliedBy(priceWFTM).dividedBy(Math.pow(10, wFTM.decimals));
-    const debtFUSDValue = debtFUSDBalance.multipliedBy(priceFUSD).dividedBy(Math.pow(10, fUSD.decimals));
-
-    const cRatio = collateralValue.dividedBy(debtFUSDValue);
-    console.log(`${cRatio.toNumber().toFixed(4)}`);
+const main = async () => {
+    await getCollateralDebtReceipt('0x228f23A962D1ACabB1775E31FAF7D1B5bfa85B5E');
+    //await tokenRegistry();
 };
 
 main().catch((e) => {
